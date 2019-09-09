2fcd92ca26d18899d825ff6b70bfd793f5d7fa85
Owen Lamont <owenrlamont@gmail.com>
2019-09-09 22:04:51 +0800

    Some initial work to get text fading in

--- tears_in_terrain.py
+++ tears_in_terrain.py
@@ -1,7 +1,8 @@
-import matplotlib.pyplot as plt
-
-from matplotlib.animation import FFMpegFileWriter
 from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FFMpegFileWriter
 
 BACKGROUND_COLOUR = "#000000FF"
 FRAME_RATE = 24
@@ -10,15 +11,19 @@ FRAME_RATE = 24
 
 
 def main():
-    anim_file_path = Path(".")
+    anim_file_path = Path("./test.mp4")
     figure = plt.figure(figsize=(19.2, 10.8))
+    alpha_array = np.linspace(0,1,100)
     # map_axes = figure.add_axes([0.0, 0.0, 1.0, 0.6])
 
     file_writer = FFMpegFileWriter(fps=FRAME_RATE)
     with file_writer.saving(figure, anim_file_path, dpi=100):
-
-        file_writer.grab_frame(facecolor=BACKGROUND_COLOUR)
-        pass
+        for alpha in alpha_array:
+            figure.clear()
+            text_axes = figure.add_axes([0.0, 0.0, 1.0, 1.0])
+            text_axes.axis("off")
+            text_axes.text(0.5,0,s="I have seen things you people would not believe", fontsize=48, style="oblique", ha="center", va="bottom", color="white", alpha=alpha)
+            file_writer.grab_frame(facecolor=BACKGROUND_COLOUR)
 
 
 if __name__ == "__main__":
