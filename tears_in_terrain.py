4ea26e26981c8443c1191c910c9f0924e7eb95e7
Owen Lamont <owenrlamont@gmail.com>
2019-09-12 22:25:21 +0800

    Tweaked the fade in (tried word by word but didn't like the effect).

--- tears_in_terrain.py
+++ tears_in_terrain.py
@@ -7,14 +7,11 @@ from matplotlib.animation import FFMpegFileWriter
 BACKGROUND_COLOUR = "#000000FF"
 FRAME_RATE = 24
 
-# class
-
 
 def main():
     anim_file_path = Path("./test.mp4")
     figure = plt.figure(figsize=(19.2, 10.8))
-    alpha_array = np.linspace(0,1,100)
-    # map_axes = figure.add_axes([0.0, 0.0, 1.0, 0.6])
+    alpha_array = np.power(np.linspace(0, 1, 50), 2)
 
     file_writer = FFMpegFileWriter(fps=FRAME_RATE)
     with file_writer.saving(figure, anim_file_path, dpi=100):
@@ -22,7 +19,17 @@ def main():
             figure.clear()
             text_axes = figure.add_axes([0.0, 0.0, 1.0, 1.0])
             text_axes.axis("off")
-            text_axes.text(0.5,0,s="I have seen things you people would not believe", fontsize=48, style="oblique", ha="center", va="bottom", color="white", alpha=alpha)
+            text_axes.text(
+                0.5,
+                0,
+                s="I have seen things you people would not believe",
+                fontsize=48,
+                style="oblique",
+                ha="center",
+                va="bottom",
+                color="white",
+                alpha=alpha,
+            )
             file_writer.grab_frame(facecolor=BACKGROUND_COLOUR)
 
 
