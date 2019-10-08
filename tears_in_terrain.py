2b7f6124f20d189cc5a7058948dbbd498a57fdb8
Owen Lamont <owenrlamont@gmail.com>
2019-10-08 12:24:22 +0800

    Added logic to fade in sections of a sentence at a time.

--- tears_in_terrain.py
+++ tears_in_terrain.py
@@ -6,6 +6,7 @@ from matplotlib.animation import FFMpegFileWriter
 
 BACKGROUND_COLOUR = "#000000FF"
 FRAME_RATE = 24
+LEFT_ALIGN = 0.08
 
 
 def main():
@@ -15,22 +16,39 @@ def main():
 
     file_writer = FFMpegFileWriter(fps=FRAME_RATE)
     with file_writer.saving(figure, anim_file_path, dpi=100):
-        for alpha in alpha_array:
-            figure.clear()
-            text_axes = figure.add_axes([0.0, 0.0, 1.0, 1.0])
-            text_axes.axis("off")
-            text_axes.text(
-                0.5,
-                0,
-                s="I have seen things you people would not believe",
-                fontsize=48,
-                style="oblique",
-                ha="center",
-                va="bottom",
-                color="white",
-                alpha=alpha,
-            )
-            file_writer.grab_frame(facecolor=BACKGROUND_COLOUR)
+        text_pos_list = [19, 47]
+        sentence = "I have seen things you people would not believe"
+        for idx, text_pos in enumerate(text_pos_list):
+            for alpha in alpha_array:
+                figure.clear()
+                text_axes = figure.add_axes([0.0, 0.0, 1.0, 1.0])
+                text_axes.axis("off")
+
+                if idx > 0:
+                    text_axes.text(
+                        LEFT_ALIGN,
+                        0,
+                        s=sentence[:text_pos_list[idx-1]],
+                        fontsize=48,
+                        style="oblique",
+                        ha="left",
+                        va="bottom",
+                        color="white",
+                        alpha=1.0,
+                    )
+
+                text_axes.text(
+                    LEFT_ALIGN,
+                    0,
+                    s=sentence[:text_pos],
+                    fontsize=48,
+                    style="oblique",
+                    ha="left",
+                    va="bottom",
+                    color="white",
+                    alpha=alpha,
+                )
+                file_writer.grab_frame(facecolor=BACKGROUND_COLOUR)
 
 
 if __name__ == "__main__":
